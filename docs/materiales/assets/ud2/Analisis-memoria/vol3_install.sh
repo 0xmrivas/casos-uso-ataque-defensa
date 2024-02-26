c24a5011bc2c7749894319b4a12eb3575888f321
Manuel R <majerisa@gmail.com>
2024-02-26 22:40:28 +0100

    initial commit

--- docs/materiales/assets/ud2/Analisis-memoria/vol3_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo apt install -y python3 python3-dev libpython3-dev python3-pip python3-setuptools python3-wheel
-python3 -m pip install -U distorm3 yara pycrypto pillow openpyxl ujson pytz ipython capstone
-python3 -m pip install -U git+https://github.com/volatilityfoundation/volatility3.git
